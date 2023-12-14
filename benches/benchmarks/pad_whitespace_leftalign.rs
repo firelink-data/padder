340e8be25d80d3ccb213684f3907c71d7bf1918d
Wilhelm Ågren <36638274+wilhelmagren@users.noreply.github.com>
2023-12-14 16:45:15 +0100

    [feat] document and improve padding functionality (#6)
    * [bench] update padding benchmarks with unwraps
    
    * [feat] add slice to fit functionality and logging
    
    * [build] logging deps

--- benches/benchmarks/pad_whitespace_leftalign.rs
+++ benches/benchmarks/pad_whitespace_leftalign.rs
@@ -5,14 +5,14 @@ use padder::Alignment;
 pub fn pad_whitespace_10_leftalign(c: &mut Criterion) {
     let width: usize = 10;
     c.bench_function("pad ws 10 la", |b| {
-        b.iter(|| black_box(whitespace("hej", width, Alignment::Left)))
+        b.iter(|| black_box(whitespace("hej", width, Alignment::Left).unwrap()))
     });
 }
 
 pub fn pad_whitespace_100_leftalign(c: &mut Criterion) {
     let width: usize = 100;
     c.bench_function("pad ws 100 la", |b| {
-        b.iter(|| black_box(whitespace("bingbong", width, Alignment::Left)))
+        b.iter(|| black_box(whitespace("bingbong", width, Alignment::Left).unwrap()))
     });
 }
 
@@ -20,11 +20,7 @@ pub fn pad_whitespace_1000_leftalign(c: &mut Criterion) {
     let width: usize = 1000;
     c.bench_function("pad ws 1000 la", |b| {
         b.iter(|| {
-            black_box(whitespace(
-                "Undercity is a cool capital...",
-                width,
-                Alignment::Left,
-            ))
+            black_box(whitespace("Undercity is a cool capital...", width, Alignment::Left).unwrap())
         })
     });
 }
@@ -33,11 +29,14 @@ pub fn pad_whitespace_10000_leftalign(c: &mut Criterion) {
     let width: usize = 10000;
     c.bench_function("pad ws 10000 la", |b| {
         b.iter(|| {
-            black_box(whitespace(
-                "¤)(åäöåa this is a very long string... xd",
-                width,
-                Alignment::Left,
-            ))
+            black_box(
+                whitespace(
+                    "¤)(åäöåa this is a very long string... xd",
+                    width,
+                    Alignment::Left,
+                )
+                .unwrap(),
+            )
         })
     });
 }
